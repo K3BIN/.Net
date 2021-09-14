afe249eb14ccea51375ac0033acd8ee654727e48
K3BIN <71346566+K3BIN@users.noreply.github.com>
2021-09-14 00:20:25 -0500

    Colecciones, Herencia, Hilos
    Ejemplo simple solo con fines demostrativos

---
+++ Personaje/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Personaje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Enemigo p = new Enemigo();
+           p.Enemy();
+        }
+    }
+}
