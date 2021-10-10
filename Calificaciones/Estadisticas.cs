13f379a6d5456f3e47fcf810c1501c8e726719ea
K3BIN <71346566+K3BIN@users.noreply.github.com>
2021-10-09 23:05:03 -0500

    Entregable 3
    Parte 1/3

---
+++ Calificaciones/Estadisticas.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calificaciones
+{
+    public class Estadisticas
+    {
+        private Alumno[] alumno;
+
+        public Estadisticas(Alumno[] alumno)
+        {
+            this.Alumno = alumno;
+        }
+
+        public Alumno[] Alumno { get => alumno; set => alumno = value; }
+        
+        public int NumAprobados()
+        {
+            int NA = 0;
+            foreach(Alumno a in alumno)
+            {
+                if(a.Calificacion >= 6)
+                {
+                    NA++;
+                }
+            }
+            return NA*10;
+        }
+        public int NumReprobados()
+        {
+            int NR = 0;
+            foreach (Alumno a in alumno)
+            {
+                if (a.Calificacion < 6)
+                {
+                    NR++;
+                }
+            }
+            return NR * 10;
+        }
+        public float Promedio()
+        {
+            float prom = 0;
+            foreach (Alumno a in alumno)
+            {
+                prom+=a.Calificacion;
+            }
+            return prom/10;
+        }
+        public  float CalificacionMinima()
+        {
+            int[] arr= new int[10];
+            int i = 0;
+            foreach (Alumno a in alumno){
+                arr[i] = a.Calificacion;
+                i++;
+            }
+
+            return arr.Min();
+        }
+        public float CalificacionMaxima()
+        {
+            int[] arr = new int[10];
+            int i = 0;
+            foreach (Alumno a in alumno)
+            {
+                arr[i] = a.Calificacion;
+                i++;
+            }
+
+            return arr.Max();
+        }
+    }
+}
